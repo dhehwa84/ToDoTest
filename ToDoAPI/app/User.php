7dff86ad51a4f11b10db8375bd4a0cc2960a93e0
dhehwa84 <dhehwa84@gmail.com>
2020-04-20 16:51:05 +0200

    added a thumbnail to user model

--- ToDoAPI/app/User.php
+++ ToDoAPI/app/User.php
@@ -18,7 +18,7 @@ class User extends  Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name','surname', 'email', 'password','image'
+        'name','surname', 'email', 'password','image', 'thumbnail'
     ];
 
     /**
