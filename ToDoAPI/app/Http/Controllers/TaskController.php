27ca1db2bbd9120dda3944441a5cd9b75a15aa83
dhehwa84 <dhehwa84@gmail.com>
2020-04-22 22:01:34 +0200

    added json responses
     - added more detail to make the responses more readable and also to easily identiy errors
     - added documentatio
     - removed unused functions

--- ToDoAPI/app/Http/Controllers/TaskController.php
+++ ToDoAPI/app/Http/Controllers/TaskController.php
@@ -7,32 +7,98 @@ use App\Task;
 
 class TaskController extends Controller
 {
-    public function index(){
-        return Task::all();
-    }
+    /**
+     * returns all tasks that were created by a user
+     * @param $id - takes  user id as param
+     * @return JSON object
+     */
     public function show($id){
-        // return Task::find($id);
-        return Task::where('created_by', '=', $id)->get();
+        $tasks = Task::where('created_by', '=', $id)->get();
+        
+        if(sizeof($tasks) > 0) {
+            return response()->json([
+                'message' => 'successully retrieved tasks',
+                'tasks' => $tasks
+            ], 201);
+        }
+        else {
+            return response()->json([
+                'error' => 'you do not have any tasks, click the + button to add.'
+            ], 404);
+        }
     }
+     /**
+     * returns a task
+     * @param $id - takes  task id as param
+     * @return JSON object
+     */
     public function showtask($id){
-        return Task::find($id);
+        $task = Task::find($id);
+        if($task) {
+            return response()->json([
+                'message' => 'successully created task',
+                'task' => $task
+            ], 201);
+        }
+        else {
+            return response()->json([
+                'error' => 'failed to add task'
+            ], 404);
+        }
 
     }
+    /**
+     * saves a new task
+     */
     public function store(Request $request){
 
-       return Task::create($request->all());
+       if(Task::create($request->all())) {
+        return response()->json([
+            'message' => 'successully created task'
+        ], 201);
     }
-    public function update(Request $request, $id){
-        $task = Task::findOrFail($id);
-        $task->update($request->all());
+    else {
+        return response()->json([
+            'error' => 'failed to add task'
+        ], 404);
+    }
+    }
+    
+    /**
+     * updates a function
+     */
+    public function update(Request $request){
+        $task = Task::findOrFail($request['id']);
+   
+        if( $task->update($request->all())) {
+            return response()->json([
+                'message' => 'successully updated task'
+            ], 201);
+        }
+        else {
+            return response()->json([
+                'error' => 'failed to update task'
+            ], 404);
+        }
 
-        return $task;
     }
+    /**
+     * deletes a task
+     * @param $id - takes the task ID as parameter
+     */
     function delete($id){
         $task = Task::findORFail($id);
-        $task->delete();
 
-        return 204;
+        if($task->delete()) {
+            return response()->json([
+                'message' => 'successully deleted task: '.$task->title
+            ], 201);
+        }
+        else {
+            return response()->json([
+                'error' => 'failed to delete task'
+            ], 404);
+        }
     }
 
 }
