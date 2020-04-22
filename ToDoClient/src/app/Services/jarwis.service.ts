5a5985a307a99cbca391657843d948263bedc4d4
dhehwa84 <dhehwa84@gmail.com>
2020-04-22 21:31:55 +0200

    documented code and removed unnecessary http requests

--- ToDoClient/src/app/Services/jarwis.service.ts
+++ ToDoClient/src/app/Services/jarwis.service.ts
@@ -11,34 +11,99 @@ export class JarwisService {
   private baseUrl = 'http://localhost:8000/api';
   constructor(private http: HttpClient) { }
 
+  /**
+   * sign up a user/ register
+   *
+   * @param {*} data - form data object
+   * @returns
+   * @memberof JarwisService
+   */
   signup(data) {
     return this.http.post(`${this.baseUrl}/signup`, data);
   }
+  /**
+   * login request
+   *
+   * @param {*} data - form data object
+   * @returns
+   * @memberof JarwisService
+   */
   login(data) {
     return this.http.post(`${this.baseUrl}/login`, data);
   }
   sendPasswordResetLink(data) {
     return this.http.post(`${this.baseUrl}/sendPasswordResetLink`, data);
   }
+  /**
+   * creates a new task
+   *
+   * @param {*} data - form data object
+   * @returns 
+   * @memberof JarwisService
+   */
   newTask(data) {
     return this.http.post(`${this.baseUrl}/task`, data);
   }
-  getTask(data) {
-    return this.http.get(`${this.baseUrl}/tasks/${data}`);
+  /**
+   * get tasks for a user
+   *
+   * @param {*} userId - creator id
+   * @returns
+   * @memberof JarwisService
+   */
+  getTask(userId) {
+    return this.http.get(`${this.baseUrl}/tasks/${userId}`);
   }
-  getUser(data) {
-    return this.http.get(`${this.baseUrl}/me/${data}`);
+  /**
+   * get user details
+   *
+   * @param {*} id
+   * @returns
+   * @memberof JarwisService
+   */
+  getUser(id) {
+    return this.http.get(`${this.baseUrl}/me/${id}`);
   }
+  /**
+   * update user details
+   *
+   * @param {*} data - form object
+   * @param {*} id - user id
+   * @returns
+   * @memberof JarwisService
+   */
   updateUser(data, id) {
     return this.http.post(`${this.baseUrl}/update/${id}`, data);
   }
+  /**
+   * set user profile image
+   *
+   * @param {*} data
+   * @param {*} id
+   * @returns - profile image url and thumbnail url
+   * @memberof JarwisService
+   */
   setImage(data, id) {
     return this.http.post(`${this.baseUrl}/setimage/${id}`, data);
   }
-  updateTaskStatus(data, id) {
-    return this.http.post(`${this.baseUrl}/task/${id}`, data);
+  /**
+   * updates a task
+   *
+   * @param {*} data - task form data
+   * @returns
+   * @memberof JarwisService
+   */
+  updateTask(data) {
+    return this.http.post(`${this.baseUrl}/updateTask`, data);
   }
+  /**
+   * removes a task
+   *
+   * @param {*} id
+   * @returns
+   * @memberof JarwisService
+   */
   deleteTask(id) {
-    return this.http.delete(`${this.baseUrl}/task/${id}`);
+    return this.http.delete(`${this.baseUrl}/deleteTask/${id}`);
   }
 }
